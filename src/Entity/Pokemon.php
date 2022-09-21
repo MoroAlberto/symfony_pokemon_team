b76cd19246bc5ca175da0561f1808c9eeb6656f3
Alberto <alberto.moro94@gmail.com>
2022-09-21 18:10:40 +0200

    create pokemon and team Entity

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PokemonRepository::class)]
+class Pokemon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?int $base_experience = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sprite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getBaseExperience(): ?int
+    {
+        return $this->base_experience;
+    }
+
+    public function setBaseExperience(int $base_experience): self
+    {
+        $this->base_experience = $base_experience;
+
+        return $this;
+    }
+
+    public function getSprite(): ?string
+    {
+        return $this->sprite;
+    }
+
+    public function setSprite(string $sprite): self
+    {
+        $this->sprite = $sprite;
+
+        return $this;
+    }
+}
