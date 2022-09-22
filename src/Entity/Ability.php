e8794be106a05a0fffdb414225e41da718a73da6
Alberto <alberto.moro94@gmail.com>
2022-09-22 15:29:01 +0200

    added Ability.php and Type.php

--- src/Entity/Ability.php
+++ src/Entity/Ability.php
@@ -2,13 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\TeamRepository;
+use App\Repository\AbilityRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: TeamRepository::class)]
-class Team
+#[ORM\Entity(repositoryClass: AbilityRepository::class)]
+class Ability
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -18,13 +18,7 @@ class Team
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $created_at = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?\DateTimeImmutable $updated_at = null;
-
-    #[ORM\OneToMany(mappedBy: 'team', targetEntity: Pokemon::class)]
+    #[ORM\ManyToMany(targetEntity: Pokemon::class, inversedBy: 'abilities')]
     private Collection $pokemon;
 
     public function __construct()
@@ -49,30 +43,6 @@ class Team
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updated_at;
-    }
-
-    public function setUpdatedAt(?\DateTimeImmutable $updated_at): self
-    {
-        $this->updated_at = $updated_at;
-
-        return $this;
-    }
-
     /**
      * @return Collection<int, Pokemon>
      */
@@ -85,7 +55,6 @@ class Team
     {
         if (!$this->pokemon->contains($pokemon)) {
             $this->pokemon->add($pokemon);
-            $pokemon->setTeam($this);
         }
 
         return $this;
@@ -93,12 +62,7 @@ class Team
 
     public function removePokemon(Pokemon $pokemon): self
     {
-        if ($this->pokemon->removeElement($pokemon)) {
-            // set the owning side to null (unless already changed)
-            if ($pokemon->getTeam() === $this) {
-                $pokemon->setTeam(null);
-            }
-        }
+        $this->pokemon->removeElement($pokemon);
 
         return $this;
     }
--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PokemonRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PokemonRepository::class)]
@@ -24,6 +26,21 @@ class Pokemon
 
     #[ORM\Column(length: 255)]
     private ?string $sprite = null;
+
+    #[ORM\ManyToMany(targetEntity: Ability::class, mappedBy: 'pokemon')]
+    private Collection $abilities;
+
+    #[ORM\ManyToMany(targetEntity: Type::class, mappedBy: 'pokemon')]
+    private Collection $types;
+
+    #[ORM\ManyToOne(inversedBy: 'pokemon')]
+    private ?Team $team = null;
+
+    public function __construct()
+    {
+        $this->abilities = new ArrayCollection();
+        $this->types = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -74,6 +91,72 @@ class Pokemon
     public function setSprite(string $sprite): self
     {
         $this->sprite = $sprite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ability>
+     */
+    public function getAbilities(): Collection
+    {
+        return $this->abilities;
+    }
+
+    public function addAbility(Ability $ability): self
+    {
+        if (!$this->abilities->contains($ability)) {
+            $this->abilities->add($ability);
+            $ability->addPokemon($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAbility(Ability $ability): self
+    {
+        if ($this->abilities->removeElement($ability)) {
+            $ability->removePokemon($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Type>
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(Type $type): self
+    {
+        if (!$this->types->contains($type)) {
+            $this->types->add($type);
+            $type->addPokemon($this);
+        }
+
+        return $this;
+    }
+
+    public function removeType(Type $type): self
+    {
+        if ($this->types->removeElement($type)) {
+            $type->removePokemon($this);
+        }
+
+        return $this;
+    }
+
+    public function getTeam(): ?Team
+    {
+        return $this->team;
+    }
+
+    public function setTeam(?Team $team): self
+    {
+        $this->team = $team;
 
         return $this;
     }
