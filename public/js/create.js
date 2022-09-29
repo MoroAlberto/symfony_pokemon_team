5be475ebc98738b141636f21943a1605857b39f3
Alberto <alberto.moro94@gmail.com>
2022-09-29 17:49:54 +0200

    replace jquery with javascript

--- public/js/create.js
+++ public/js/create.js
@@ -1,11 +1,7 @@
 function addPokemonToTeam() {
     $.getJSON("/pokemon/new", function (data) {
         let i;
-        let pokemon = '<img src="' + data.sprite + '" class="card-img-top" alt="' + data.name + '">' +
-            '<div class="card-body">' +
-            '<h5 class="card-title">' + data.name + '</h5>' +
-            '<h6 class="card-subtitle mb-2 text-muted">' + data.baseExperience + '</h6>' +
-            '<p class="card-text">Abilities:';
+        let pokemon = '<img src="' + data.sprite + '" class="card-img-top" alt="' + data.name + '">' + '<div class="card-body">' + '<h5 class="card-title">' + data.name + '</h5>' + '<h6 class="card-subtitle mb-2 text-muted">' + data.baseExperience + '</h6>' + '<p class="card-text">Abilities:';
         for (i = 0; i < data.abilities.length;) {
             pokemon += ' ' + data.abilities[i]['name'];
             i++;
@@ -24,28 +20,35 @@ function addPokemonToTeam() {
         }
         pokemon.substring(0, pokemon.length - 1);
         pokemon += '</p></div>';
-        const slot = $("#PokemonTeam div.free").first();
-        if (slot.length <= 0) {
+        const innerDiv = document
+            .querySelector('#PokemonTeam')
+            .getElementsByClassName('free')
+        if (innerDiv.length <= 0) {
             alert("Error: A trainer cannot own more than 6 pokemon");
+            return false;
         }
-        slot.removeClass("free");
-        slot.html(pokemon);
-        const HiddenPokemonList = $("#team_ajaxString");
-        if (HiddenPokemonList.val() === "") {
-            HiddenPokemonList.val(JSON.stringify(data));
+        innerDiv[0].innerHTML = '';
+        console.log(innerDiv);
+        innerDiv[0].innerHTML = pokemon;
+        const HiddenPokemonList = document.getElementById("team_ajaxString");
+        if (HiddenPokemonList.value === "") {
+            HiddenPokemonList.value = JSON.stringify(data);
         } else {
-            HiddenPokemonList.val(HiddenPokemonList.val() + "," + JSON.stringify(data));
+            HiddenPokemonList.value = HiddenPokemonList.value + "," + JSON.stringify(data);
         }
+        innerDiv[0].classList.remove("free");
     });
 }
 
-$("form#formTeam").submit(function (event) {
-    event.preventDefault();
-    const form = this;
-    const slot = $("#PokemonTeam div.free");
-    if (slot.length >= 6) {
+function validateForm() {
+    const innerDiv = document
+        .querySelector('#PokemonTeam')
+        .getElementsByClassName('free')
+    if (innerDiv.length >= 6) {
         alert("Error: Please add at least one Pokemon on your team. Pressing the button Gotta Catch 'Em All");
         return false;
     }
-    form.submit();
-});
+}
+
+
+
